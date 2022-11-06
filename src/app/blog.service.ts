e5a62b27fbe25b49999cd6ffa577ba56c35c3650
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-06 21:59:12 +0530

    Imported http client in blog service and added httpclient to constructor

--- src/app/blog.service.ts
+++ src/app/blog.service.ts
@@ -1,11 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Blog } from './model/blog';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
 
+  constructor(private httpClient:HttpClient) { }
+
   URL: string = "http://localhost:3000/blogs"
-  
-  constructor() { }
+
+  // fetchBlogs():Observable<Array<Blog>>{
+  //   return this.
+  // }
+
 }
