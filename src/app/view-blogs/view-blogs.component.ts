3b2f97d9b819b9d72e7a7ecae37d7dfa92dadfce
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-07 00:47:58 +0530

    formatted the code

--- src/app/view-blogs/view-blogs.component.ts
+++ src/app/view-blogs/view-blogs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogService } from '../blog.service';
+import { Blog } from '../model/blog';
 
 @Component({
   selector: 'app-view-blogs',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewBlogsComponent implements OnInit {
 
-  constructor() { }
+  // Blogservice in the constructor parameter
+  constructor(private blogService: BlogService) { }
 
+  // declare a variable to store the data from the json server
+  blogs: Blog[] = [];
+
+  /**
+   * This method calls the fetchBlogs from blog service.
+   * the fetchBlogs calls the data from json server.
+   * store the fetched data in the blog variable in an array form.
+   */
   ngOnInit(): void {
+    this.blogService.fetchBlogs().subscribe({
+      next: (data) => this.blogs = data,
+      error: () => alert("Failed to Fetch Blogs due to Network Error!!!"),
+      complete: () => alert("Blog Data Fetched Successfully!!!")
+    })
   }
-
 }
