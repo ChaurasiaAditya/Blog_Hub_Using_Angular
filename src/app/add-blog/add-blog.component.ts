088562c49195df73e8e3395cceb742f7f8089291
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-07 01:24:09 +0530

    added addBlog function in add blog component

--- src/app/add-blog/add-blog.component.ts
+++ src/app/add-blog/add-blog.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { BlogService } from '../blog.service';
+import { Blog } from '../model/blog';
 
 @Component({
   selector: 'app-add-blog',
   templateUrl: './add-blog.component.html',
   styleUrls: ['./add-blog.component.css']
 })
-export class AddBlogComponent implements OnInit {
+export class AddBlogComponent {
 
-  constructor() { }
+  constructor(private blogService: BlogService) { }
 
-  ngOnInit(): void {
+  // variable to store the blog from the input
+  blog: Blog = {};
+
+  /**
+   * This method Add the blog to the json file using HTTP client.
+   * it takes the blog object.
+   */
+  addBlog() {
+    this.blogService.addBlogs(this.blog).subscribe({
+      next: () => alert("Blog Added"),
+      error: () => alert("Failed to Add Blogs due to Network Error!!!")
+    })
   }
-
 }
