783af4d6e885bc02ae58caee4d1bf9ddf12d0ddc
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-06 22:11:37 +0530

    Added BlobService in parameter of constructor

--- src/app/view-blogs/view-blogs.component.ts
+++ src/app/view-blogs/view-blogs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogService } from '../blog.service';
 
 @Component({
   selector: 'app-view-blogs',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewBlogsComponent implements OnInit {
 
-  constructor() { }
+  // Added BlobService in parameter of constructor
+  constructor(private blogService:BlogService) { }
 
   ngOnInit(): void {
   }
